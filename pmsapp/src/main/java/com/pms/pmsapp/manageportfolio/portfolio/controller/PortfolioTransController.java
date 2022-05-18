526b679f6f8c086032ac9f8c125d58e0b0677cdd
Vincent Lai <vincentlai91@hotmail.com>
2022-05-18 16:40:55 +0800

    refactor transaction getting stock exchange details: 1. added NASDAQGM and NASDAQCM 2. Initialise HashMap on static class

--- pmsapp/src/main/java/com/pms/pmsapp/manageportfolio/portfolio/controller/PortfolioTransController.java
+++ pmsapp/src/main/java/com/pms/pmsapp/manageportfolio/portfolio/controller/PortfolioTransController.java
@@ -1,10 +1,11 @@
 package com.pms.pmsapp.manageportfolio.portfolio.controller;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.pms.pmsapp.manageportfolio.dividend.service.DividendService;
+import com.pms.pmsapp.manageportfolio.portfolio.data.MktExchg;
+import com.pms.pmsapp.manageportfolio.portfolio.data.PortfolioTrans;
+import com.pms.pmsapp.manageportfolio.portfolio.data.StockWrapper;
+import com.pms.pmsapp.manageportfolio.portfolio.service.PortfolioHoldService;
+import com.pms.pmsapp.manageportfolio.portfolio.service.PortfolioTransService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +14,12 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.pms.pmsapp.manageportfolio.dividend.service.DividendService;
-import com.pms.pmsapp.manageportfolio.portfolio.data.MktExchg;
-import com.pms.pmsapp.manageportfolio.portfolio.data.PortfolioTrans;
-import com.pms.pmsapp.manageportfolio.portfolio.data.StockWrapper;
-import com.pms.pmsapp.manageportfolio.portfolio.service.PortfolioHoldService;
-import com.pms.pmsapp.manageportfolio.portfolio.service.PortfolioTransService;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 @RestController
@@ -45,18 +38,33 @@ public class PortfolioTransController {
 
 	private final String SELL_ACTION = "SELL";
 
-	private final String EXCHG_HKEX = "HKSE";
-	private final String EXCHG_SGX = "SES";
-	private final String EXCHG_NYSE = "NYSE";
-	private final String EXCHG_NYSE_ARCA = "NYSEArca";
-	private final String EXCHG_NASDAQ = "NasdaqGS";
-	private final String EXCHG_OTC = "Other OTC";
+	private static final String EXCHG_HKEX = "HKSE";
+	private static final String EXCHG_SGX = "SES";
+	private static final String EXCHG_NYSE = "NYSE";
+	private static final String EXCHG_NYSE_ARCA = "NYSEArca";
+	private static final String EXCHG_NASDAQ_GS = "NasdaqGS";
+  private static final String EXCHG_NASDAQ_GM = "NasdaqGM";
+  private static final String EXCHG_NASDAQ_CM = "NasdaqCM";
+	private static final String EXCHG_OTC = "Other OTC";
 
-	private final String RENAM_EXCHG_HKEX = "HKEX";
-	private final String RENAM_EXCHG_SGX = "SGX";
-	private final String RENAM_EXCHG_NYSE = "NYSE";
-	private final String RENAM_EXCHG_NASDAQ = "NASDAQ";
-	private final String RENAM_EXCHG_OTC = "OTC";
+	private static final String RENAM_EXCHG_HKEX = "HKEX";
+	private static final String RENAM_EXCHG_SGX = "SGX";
+	private static final String RENAM_EXCHG_NYSE = "NYSE";
+	private static final String RENAM_EXCHG_NASDAQ = "NASDAQ";
+	private static final String RENAM_EXCHG_OTC = "OTC";
+
+  public static Map<String, String> exchgSuffmap;
+  static {
+    exchgSuffmap = new HashMap<>();
+    exchgSuffmap.put(EXCHG_HKEX, RENAM_EXCHG_HKEX);
+    exchgSuffmap.put(EXCHG_SGX, RENAM_EXCHG_SGX);
+    exchgSuffmap.put(EXCHG_NYSE, RENAM_EXCHG_NYSE);
+    exchgSuffmap.put(EXCHG_NYSE_ARCA, RENAM_EXCHG_NYSE);
+    exchgSuffmap.put(EXCHG_NASDAQ_GS, RENAM_EXCHG_NASDAQ);
+    exchgSuffmap.put(EXCHG_NASDAQ_GM, RENAM_EXCHG_NASDAQ);
+    exchgSuffmap.put(EXCHG_NASDAQ_CM, RENAM_EXCHG_NASDAQ);
+    exchgSuffmap.put(EXCHG_OTC, RENAM_EXCHG_OTC);
+  }
 
 	@RequestMapping(value="/portfolio/transaction/{portId}", method=RequestMethod.GET)
 	public Page<PortfolioTrans> findAll(@RequestParam("page") int page, @RequestParam("size") int size,
@@ -123,13 +131,15 @@ public class PortfolioTransController {
 	public PortfolioTrans retrieveStockInfo(@RequestBody PortfolioTrans portfolioTrans) {
 		log.info("retrieveStockInfo Trans in Controller");
 
-		Map<String, String> exchgSuffmap = new HashMap<String, String>();
-		exchgSuffmap.put(EXCHG_HKEX, RENAM_EXCHG_HKEX);
-		exchgSuffmap.put(EXCHG_SGX, RENAM_EXCHG_SGX);
-		exchgSuffmap.put(EXCHG_NYSE, RENAM_EXCHG_NYSE);
-		exchgSuffmap.put(EXCHG_NYSE_ARCA, RENAM_EXCHG_NYSE);
-		exchgSuffmap.put(EXCHG_NASDAQ, RENAM_EXCHG_NASDAQ);
-		exchgSuffmap.put(EXCHG_OTC, RENAM_EXCHG_OTC);
+//		Map<String, String> exchgSuffmap = new HashMap<String, String>();
+//		exchgSuffmap.put(EXCHG_HKEX, RENAM_EXCHG_HKEX);
+//		exchgSuffmap.put(EXCHG_SGX, RENAM_EXCHG_SGX);
+//		exchgSuffmap.put(EXCHG_NYSE, RENAM_EXCHG_NYSE);
+//		exchgSuffmap.put(EXCHG_NYSE_ARCA, RENAM_EXCHG_NYSE);
+//		exchgSuffmap.put(EXCHG_NASDAQ_GS, RENAM_EXCHG_NASDAQ);
+//    exchgSuffmap.put(EXCHG_NASDAQ_GM, RENAM_EXCHG_NASDAQ);
+//    exchgSuffmap.put(EXCHG_NASDAQ_CM, RENAM_EXCHG_NASDAQ);
+//		exchgSuffmap.put(EXCHG_OTC, RENAM_EXCHG_OTC);
 
 		String stockSym = portfolioTrans.getStockSymbol();
 		StockWrapper stockWrapper = portfolioHoldService.findStock(stockSym);
