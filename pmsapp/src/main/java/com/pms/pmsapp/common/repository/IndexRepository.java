3db4111e0ad840568c43ae11d7ee50ab3e91e1f8
Vincent Lai <vincentlai91@hotmail.com>
2022-07-11 08:00:14 +0800

    converted query to from JPQL to native query

--- pmsapp/src/main/java/com/pms/pmsapp/common/repository/IndexRepository.java
+++ pmsapp/src/main/java/com/pms/pmsapp/common/repository/IndexRepository.java
@@ -10,6 +10,9 @@ import com.pms.pmsapp.common.data.Index;
 
 @Repository
 public interface IndexRepository extends JpaRepository<Index, Long> {
-	@Query("select new Index(id, idxSym, idxName, last, change, changePct, lastUpdatedDt) from Index order by id asc ")
+	// @Query("select new Index(id, idxSym, idxName, last, change, changePct,
+	// lastUpdatedDt) from Index order by id asc ")
+	@Query(value = "SELECT * FROM PMS_INDEX order by ID asc", nativeQuery = true)
+
 	public List<Index> findAllIndex();
 }
